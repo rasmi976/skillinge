bced8bdc92ff0e88958e2456495cdd079b81cf9d
Rasmus <rasmi976@gmail.com>
2026-07-25 21:06:24 +0200

    Initial commit: Österlen trip planning site

---
+++ sw.js
@@ -0,0 +1,34 @@
+var CACHE_NAME = 'osterlen-v2';
+var SHELL = ['./osterlen.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(c){ return c.addAll(SHELL); }).catch(function(){})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(names){
+      return Promise.all(names.filter(function(n){ return n !== CACHE_NAME; }).map(function(n){ return caches.delete(n); }));
+    }).then(function(){ return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function(e){
+  if(e.request.method !== 'GET') return;
+  var url = new URL(e.request.url);
+  if(url.origin !== location.origin) return;
+  e.respondWith(
+    fetch(e.request).then(function(res){
+      if(res && res.status === 200){
+        var copy = res.clone();
+        caches.open(CACHE_NAME).then(function(c){ c.put(e.request, copy); });
+      }
+      return res;
+    }).catch(function(){
+      return caches.match(e.request);
+    })
+  );
+});
